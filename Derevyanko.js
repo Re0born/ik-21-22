9147296c31d9b93a15810a70f5f768b8faae7a48
m1mp <GDerevinko@gmail.com>
2023-06-06 10:38:46 +0300

    add new file

---
+++ Derevyanko.js
@@ -0,0 +1,22 @@
+function convertToAmPm(hours, minutes) {
+    let period = "AM";
+    
+    if (hours >= 12) {
+      period = "PM";
+      if (hours > 12) {
+        hours -= 12;
+      }
+    }
+    
+    if (hours === 0) {
+      hours = 12;
+    }
+    
+    console.log(`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${period}`);
+  }
+  
+  const hours = parseInt(prompt("Введіть години:"));
+  const minutes = parseInt(prompt("Введіть хвилини:"));
+  
+  convertToAmPm(hours, minutes);
+  
