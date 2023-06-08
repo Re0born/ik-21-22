9d8a051aca3744f5df7218aee3ec379098fce850
PotebniaDmitry <135119599+PotebniaDmitry@users.noreply.github.com>
2023-06-08 09:55:13 +0300

    DmiteyIk2.cpp

--- DmiteyIk2.cpp
+++ DmiteyIk2.cpp
@@ -1,23 +1,16 @@
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
-{
-    const float pi = 3.1415f;
-    float r, d, s, v;
+#include <iostream>
+using namespace std;
 
-    r = r.Parse(this->textBox1->Text);
+int main() {
+    double radius;
+    double area;
 
-    switch (comboBox1->SelectedIndex)
-    {
-    case 0:
-        d = 2 * pi * r;
-        label2->Text = "Довжина кола = " + d.ToString();
-        break;
-    case 1:
-        s = pi * r * r;
-        label2->Text = "Площа круга = " + s.ToString();
-        break;
-    case 2:
-        v = 4.0 / 3.0 * pi * r * r * r;
-        label2->Text = "Об'єм кулі = " + v.ToString();
-        break;
-    }
+    cout << "Введіть радіус кола: ";
+    cin >> radius;
+
+    area = 3.14159 * radius * radius;
+
+    cout << "Площа кола: " << area << endl;
+
+    return 0;
 }
